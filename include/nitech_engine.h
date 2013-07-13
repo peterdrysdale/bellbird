bfe676fa04fc46dcd2f248009710ccdf9827ca04
Sergio Oller <sergioller@gmail.com>
2013-07-13 14:53:57 +0200

    Public header does not depend anymore on private header

--- include/nitech_engine.h
+++ include/nitech_engine.h
@@ -2,7 +2,102 @@
 #define __NITECH_ENGINE_H__
 
 #include "cst_utterance.h"
-#include "../src/nitechvoice/nitech_hidden.h"
+#include "cst_file.h"
+
+typedef struct _ModelSet { /* HMM set handler */
+   int nstate;
+   int lf0stream;
+   int mcepvsize;
+   int ndurpdf;
+   int *nmceppdf;
+   int *nlf0pdf;
+   float **durpdf,***mceppdf,****lf0pdf;
+   cst_file fp[3];
+} ModelSet;
+
+typedef struct _Pattern{  /* pattern handler for question storage */
+   char *pat;               /* pattern */
+   struct _Pattern *next;   /* link to next pattern */
+} Pattern;
+
+typedef struct _Question { /* question storage */
+   char *qName;              /* name of this question */
+   Pattern *phead;           /* link to head of pattern list */
+   Pattern *ptail;           /* link to tail of pattern list */
+   struct _Question *next;  /* link to next question */
+} Question;
+
+typedef struct _Node {     /* node of decision tree */
+   int idx;                 /* index of this node */
+   int pdf;                 /* index of pdf for this node  ( leaf node only ) */
+   struct _Node *yes;       /* link to child node (yes) */
+   struct _Node *no;        /* link to child node (no)  */
+   Question *quest;          /* question applied at this node */
+} Node;
+   
+typedef struct _Tree {      
+   int state;                 /* state position of this tree */
+   struct _Tree *next;        /* link to next tree */
+   Node *root;                 /* root node of this decision tree */
+} Tree;
+
+typedef struct _TreeSet {
+   Question *qhead[3];
+   Question *qtail[3];
+
+   Tree *thead[3];
+   Tree *ttail[3];
+   
+   cst_file fp[3];
+   
+} TreeSet;
+
+typedef struct _DWin {
+   int num;           /* number of static + deltas */
+   char **fn;         /* delta window coefficient file */
+   int **width;       /* width [0..num-1][0(left) 1(right)] */
+   float **coef;      /* coefficient [0..num-1][length[0]..length[1]] */
+   float **coefr;     /* pointers to the memory being allocated */
+   int maxw[2];       /* max width [0(left) 1(right)] */
+   int max_L;
+} DWin;
+
+typedef struct _PStream {
+   int vSize;
+   int order;
+   int T;
+   int width;
+   DWin dw;
+   double **mseq;   /* sequence of mean vector */
+   double **ivseq;	 /* sequence of inversed variance vector */
+   double **R;
+   double *r;
+   double *g;
+   float **c;     /* output parameter vector */
+} PStream;
+
+typedef struct _VocoderSetup {
+   
+   int fprd;
+   int iprd;
+   int seed;
+   int pd;
+   unsigned long next;
+   double p1;
+   double pc;
+   double pj;
+   double pade[21];
+   double *ppade;
+   double *c, *cc, *cinc, *d1;
+   double rate;
+   
+   int sw;
+   double r1, r2, s;
+   
+   int x;
+   
+} VocoderSetup;
+
 typedef struct _nitech_engine {
    cst_utterance *utt;
    ModelSet   ms;
--- src/nitechvoice/nitech_hidden.h
+++ src/nitechvoice/nitech_hidden.h
@@ -42,8 +42,11 @@
 /*                                                                   */
 /*  ---------------------------------------------------------------  */
 
-#include "cst_file.h"
+#ifndef __NITECH_HIDDEN_H__
+#define __NITECH_HIDDEN_H__
+
 #include "cst_wave.h"
+#include "nitech_engine.h"
 
 typedef struct _globalP {  
    float   RHO      ;  /* variable for speaking rate control         */   
@@ -76,16 +79,6 @@ typedef struct _UttModel { /* Utterance model handler */
    int totalframe;
 } UttModel;
 
-typedef struct _ModelSet { /* HMM set handler */
-   int nstate;
-   int lf0stream;
-   int mcepvsize;
-   int ndurpdf;
-   int *nmceppdf;
-   int *nlf0pdf;
-   float **durpdf,***mceppdf,****lf0pdf;
-   cst_file fp[3];
-} ModelSet;
 
 void LoadModelFiles (ModelSet *);
 void FindDurPDF (Model *, ModelSet *, float, int );
@@ -94,97 +87,17 @@ void FindMcpPDF (int, Model *, ModelSet *);
 void InitModelSet (ModelSet *);
 void DeleteModelSet(ModelSet *ms);
 
-typedef struct _Pattern{  /* pattern handler for question storage */
-   char *pat;               /* pattern */
-   struct _Pattern *next;   /* link to next pattern */
-} Pattern;
-
-typedef struct _Question { /* question storage */
-   char *qName;              /* name of this question */
-   Pattern *phead;           /* link to head of pattern list */
-   Pattern *ptail;           /* link to tail of pattern list */
-   struct _Question *next;  /* link to next question */
-} Question;
-
-typedef struct _Node {     /* node of decision tree */
-   int idx;                 /* index of this node */
-   int pdf;                 /* index of pdf for this node  ( leaf node only ) */
-   struct _Node *yes;       /* link to child node (yes) */
-   struct _Node *no;        /* link to child node (no)  */
-   Question *quest;          /* question applied at this node */
-} Node;
-   
-typedef struct _Tree {      
-   int state;                 /* state position of this tree */
-   struct _Tree *next;        /* link to next tree */
-   Node *root;                 /* root node of this decision tree */
-} Tree;
-
-typedef struct _TreeSet {
-   Question *qhead[3];
-   Question *qtail[3];
-
-   Tree *thead[3];
-   Tree *ttail[3];
-   
-   cst_file fp[3];
-   
-} TreeSet;
-
 void LoadTreesFile (TreeSet *, Mtype);
 int SearchTree (char *, Node *);
 void InitTreeSet(TreeSet *);
 void FreeTrees(TreeSet *ts, Mtype type);
 
-typedef struct _VocoderSetup {
-   
-   int fprd;
-   int iprd;
-   int seed;
-   int pd;
-   unsigned long next;
-   double p1;
-   double pc;
-   double pj;
-   double pade[21];
-   double *ppade;
-   double *c, *cc, *cinc, *d1;
-   double rate;
-   
-   int sw;
-   double r1, r2, s;
-   
-   int x;
-   
-} VocoderSetup;
 
 void init_vocoder(int m, VocoderSetup *vs);
 void vocoder (double p, float *mc, int m, cst_wave *w, int samp_offset, globalP *gp, VocoderSetup *vs);
 
-typedef struct _DWin {
-   int num;           /* number of static + deltas */
-   char **fn;         /* delta window coefficient file */
-   int **width;       /* width [0..num-1][0(left) 1(right)] */
-   float **coef;      /* coefficient [0..num-1][length[0]..length[1]] */
-   float **coefr;     /* pointers to the memory being allocated */
-   int maxw[2];       /* max width [0(left) 1(right)] */
-   int max_L;
-} DWin;
-
-typedef struct _PStream {
-   int vSize;
-   int order;
-   int T;
-   int width;
-   DWin dw;
-   double **mseq;   /* sequence of mean vector */
-   double **ivseq;	 /* sequence of inversed variance vector */
-   double **R;
-   double *r;
-   double *g;
-   float **c;     /* output parameter vector */
-} PStream;
-
 cst_wave * pdf2speech(PStream *, PStream *, globalP *, ModelSet *, UttModel *, VocoderSetup *);
 void ReadWin(PStream *);
 void FreeWin(PStream *);
+
+#endif
