02b895c70817cb7cfb13c0544c1d8355c015a607
Peter Drysdale <drysdalepete@gmail.com>
2016-02-17 19:22:22 +1100

    Alter spamf0 to do operation in place.

--- src/cg/cst_spamf0.c
+++ src/cg/cst_spamf0.c
@@ -48,28 +48,58 @@
 #include "cst_utt_utils.h"
 #include "bell_relation_sym.h"
 
-static void cst_synthtilt(const cst_cg_db *cg_db, 
+static void cst_synthtilt(const float frame_advance,
                           const float start, const float peak, const float tiltamp, 
                           const float tiltdur, const float tilttilt,
-                          cst_track *ftrack);
+                          float **f0_out)
+{
+    float arise,afall,drise,dfall,i;
+    int num_frames;
+    arise= tiltamp*(1+tilttilt)/2;
+    afall= tiltamp*(1-tilttilt)/2;
+    drise= tiltdur*(1+tilttilt)/2;
+    dfall= tiltdur*(1-tilttilt)/2;
+    num_frames=(int)ceil((double)(start/frame_advance));
+    // Synthesizing the rise event
+    for (i=frame_advance;((num_frames * frame_advance)<(start+(drise/2))) ; num_frames++,i+=frame_advance)
+    {
+        f0_out[num_frames][0]+= peak - arise + (2 * arise * (i/drise) * (i/drise));
+        f0_out[num_frames][0]=exp(f0_out[num_frames][0]);
+    }
+    for (;((num_frames * frame_advance)<(start+drise)) ; num_frames++,i+=frame_advance)
+    {
+        f0_out[num_frames][0]+= peak - 2 * arise * (1- (i/drise)) * (1- (i/drise));
+        f0_out[num_frames][0]=exp(f0_out[num_frames][0]);
+    }
+    // Synthesizing the fall event
+    for (i=frame_advance;((num_frames * frame_advance)<(start+drise+(dfall/2))) ; num_frames++,i+=frame_advance)
+    {
+        f0_out[num_frames][0]+= peak + afall - (2 * afall * (i/dfall) * (i/dfall)) - afall;
+        f0_out[num_frames][0]=exp(f0_out[num_frames][0]);
+    }
+    for (;((num_frames * frame_advance)<(start+drise+dfall)) ; num_frames++,i+=frame_advance)
+    {
+        f0_out[num_frames][0]+= peak + (2 * afall * (1- (i/dfall)) * (1- (i/dfall))) - afall;
+        f0_out[num_frames][0]=exp(f0_out[num_frames][0]);
+    }
+    return ;
+}
 
 cst_utterance *cst_spamf0(cst_utterance *utt)
 {
-    cst_track *spamf0_track=NULL;
-    cst_track *param_track=NULL;
+    float **f0_out=val_track(UTT_FEAT_VAL(utt,"param_track"))->frames;;
     cst_item *s;
     cst_cg_db *cg_db;
     const cst_cart *acc_tree, *phrase_tree;
-    float end,f0val, syldur;
-    int num_frames,f,i;
+    float end,f0val,syldur,start;
+    int i,f;
     cg_db = val_cg_db(UTT_FEAT_VAL(utt,"cg_db"));
     int param_track_num_frames = UTT_FEAT_INT(utt,"param_track_num_frames");
 
-    spamf0_track=new_track(param_track_num_frames, 1);
     acc_tree = cg_db->spamf0_accent_tree;
     phrase_tree = cg_db->spamf0_phrase_tree;
     end = 0.0;
-    num_frames = 0;
+    i = 0;
     for (s = UTT_REL_HEAD(utt,SEGMENT); s; s=item_next(s))
     {
         end = ffeature_float(s,"end");
@@ -82,67 +112,24 @@ cst_utterance *cst_spamf0(cst_utterance *utt)
             f0val=val_float(cart_interpret(s,phrase_tree));
         }
 
-        for ( ; ((num_frames * cg_db->frame_advance) <= end) && (num_frames < param_track_num_frames); num_frames++)
+        for (; ((i * cg_db->frame_advance) <= end) && (i < param_track_num_frames); i++)
         {
-            spamf0_track->frames[num_frames][0]=f0val;
+            f0_out[i][0]=f0val;
         }
     }
 
     for (s=UTT_REL_HEAD(utt,SYLLABLE); s; s=item_next(s))
     {
         f = val_int(cart_interpret(s,acc_tree));
-        syldur = ffeature_float(s,"R:"SYLSTRUCTURE".dn.R:"SEGMENT".end")
-            - ffeature_float(s,"R:"SYLSTRUCTURE".d1.R:"SEGMENT".p.end");
-        cst_synthtilt(cg_db,
-		      ffeature_float(s,"R:"SYLSTRUCTURE".d1.R:"SEGMENT".p.end"),
+        start = ffeature_float(s,"R:"SYLSTRUCTURE".d1.R:"SEGMENT".p.end");
+        syldur = ffeature_float(s,"R:"SYLSTRUCTURE".dn.R:"SEGMENT".end") - start;
+        cst_synthtilt(cg_db->frame_advance,
+		      start,
                       cg_db->spamf0_accent_vectors[f][0],
                       cg_db->spamf0_accent_vectors[f][2],
                       syldur,
                       cg_db->spamf0_accent_vectors[f][6],
-                      spamf0_track);
+                      f0_out);
     }
-    param_track = val_track(UTT_FEAT_VAL(utt,"param_track"));
-    for (i=0; i<param_track_num_frames; i++)
-    {
-        param_track->frames[i][0]=spamf0_track->frames[i][0];
-    }
-    delete_track(spamf0_track);
     return utt;
-}
-
-static void cst_synthtilt(const cst_cg_db *cg_db,
-                          const float start, const float peak, const float tiltamp, 
-                          const float tiltdur, const float tilttilt,
-                          cst_track *ftrack)
-{
-    float arise,afall,drise,dfall,i;
-    int num_frames;
-    arise= tiltamp*(1+tilttilt)/2;
-    afall= tiltamp*(1-tilttilt)/2;
-    drise= tiltdur*(1+tilttilt)/2;
-    dfall= tiltdur*(1-tilttilt)/2;
-    num_frames=(int)ceil((double)(start/cg_db->frame_advance));
-    // Synthesizing the rise event
-    for (i=cg_db->frame_advance;((num_frames * cg_db->frame_advance)<(start+(drise/2))) ; num_frames++,i+=cg_db->frame_advance)
-    {
-        ftrack->frames[num_frames][0]+= peak - arise + (2 * arise * (i/drise) * (i/drise));
-        ftrack->frames[num_frames][0]=exp(ftrack->frames[num_frames][0]);
-    }
-    for (;((num_frames * cg_db->frame_advance)<(start+drise)) ; num_frames++,i+=cg_db->frame_advance)
-    {
-        ftrack->frames[num_frames][0]+= peak - 2 * arise * (1- (i/drise)) * (1- (i/drise));
-        ftrack->frames[num_frames][0]=exp(ftrack->frames[num_frames][0]);
-    }
-    // Synthesizing the fall event
-    for (i=cg_db->frame_advance;((num_frames * cg_db->frame_advance)<(start+drise+(dfall/2))) ; num_frames++,i+=cg_db->frame_advance)
-    {
-        ftrack->frames[num_frames][0]+= peak + afall - (2 * afall * (i/dfall) * (i/dfall)) - afall;
-        ftrack->frames[num_frames][0]=exp(ftrack->frames[num_frames][0]);
-    }
-    for (;((num_frames * cg_db->frame_advance)<(start+drise+dfall)) ; num_frames++,i+=cg_db->frame_advance)
-    {
-        ftrack->frames[num_frames][0]+= peak + (2 * afall * (1- (i/dfall)) * (1- (i/dfall))) - afall;
-        ftrack->frames[num_frames][0]=exp(ftrack->frames[num_frames][0]);
-    }
-    return ;
 }
