1196cf59b4ab1c850646b10b41eb7f477ef280ae
Peter Drysdale <drysdalepete@gmail.com>
2016-06-16 21:50:17 +1000

    Merge data structures which are no longer common into cg code.

--- src/cg/cst_mlpg.c
+++ src/cg/cst_mlpg.c
@@ -93,10 +93,28 @@
 #include "cst_cg.h"
 #include "cst_error.h"
 #include "bell_track.h"
-#include "pstream.h"
 
 #define	WLEFT 0
 #define	WRIGHT 1
+
+typedef struct _DWin {
+   int num;           // number of static + deltas
+   int width[2];      // width [0(left) 1(right)]
+   double *coef;      // coefficient [length[0]..length[1]]
+   double *coef_ptrs; // pointers to the memory being allocated so they can be freed
+} DWin;
+
+typedef struct _PStream {
+   int T;           // number of frames
+   int width;
+   DWin dw;
+   double **mseq;   // sequence of mean vector
+   double **ivseq;  // sequence of inversed variance vector
+   double **R;
+   double *r;
+   double *g;
+   double *c;       // output parameter vector
+} PStream;
 
 // NOTE NOTE NOTE we are including static functions here not header files
 #include "../commonsynth/ldl.c"
